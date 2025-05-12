36efd4b69a4ce34852bfb0bac27a3f6201945ea1
Brandon AMT <bhernandez@appliedmfg.com>
2025-05-12 09:21:19 -0500

    url parameters. sanitized. moving forward...

--- ch_04/src/main.rs
+++ ch_04/src/main.rs
@@ -57,29 +57,33 @@ impl std::fmt::Display for QuestionId {
     }
 }
 
+// Route handler!
 async fn get_questions(
     params: HashMap<String, String>,
     store: Store,
 ) -> Result<impl warp::Reply, warp::Rejection> {
-    println!("{:?}", params);
-
-    let mut start = 0;
-
-    if let Some(n) = params.get("start") {
-        start = n.parse::<usize>().expect("Failed to parse start!");
+   
+    if !params.is_empty() {
+        let res: Vec<Question> = store.questions.values().cloned().collect();
+        let pagination = extract_pagination(params)?;
+        let res = &res[pagination.start..pagination.end];
+        Ok(warp::reply::json(&res))
+    } else {
+        let res: Vec<Question> = store.questions.values().cloned().collect();
+        // To get the whole Struct
+        // let res = store;
+        Ok(warp::reply::json(&res))
     }
 
-    println!("{}", start);
-    panic!("aaaaaaaaaaaahhhh");
-
-    let res: Vec<Question> = store.questions.values().cloned().collect();
-    // To get whole Struct
-    // let res = store;
-    Ok(warp::reply::json(&res))
 }
 
 async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
-    if let Some(error) = r.find::<CorsForbidden>() {
+    if let Some(error) = r.find::<Error>() {
+        Ok(warp::reply::with_status(
+            error.to_string(),
+            StatusCode::RANGE_NOT_SATISFIABLE,
+        ))
+    } else if let Some(error) = r.find::<CorsForbidden>() {
         Ok(warp::reply::with_status(
             error.to_string(),
             StatusCode::FORBIDDEN,
@@ -90,6 +94,72 @@ async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
             StatusCode::NOT_FOUND,
         ))
     }
+}
+
+#[derive(Debug)]
+enum Error {
+    ParseError(std::num::ParseIntError),
+    MissingParameters,
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            Error::ParseError(ref err) => {
+                write!(f, "Cannot parse parameter: {}", err)
+            },
+            Error::MissingParameters => {
+                write!(f, "Missing parameter")
+            },
+        }
+    }
+}
+
+// Marker trait. This allows the Error to be returned in a Warp route handler.
+impl Reject for Error {}
+
+#[derive(Debug)]
+struct Pagination {
+    start: usize,
+    end: usize,
+}
+
+// This one was my idea
+impl Pagination {
+    fn sanitize(mut self) -> Self {
+        if self.start > self.end {
+            let saved_start = self.start;
+            self.start = self.end;
+            self.end = saved_start;
+        }
+        self
+    }
+    fn verify_range(mut self, max_len: usize) -> () {
+        
+    }
+}
+
+
+fn extract_pagination(params: HashMap<String, String>) -> Result<Pagination, Error> {
+    if params.contains_key("start") && params.contains_key("end") {
+        let mut pagination = Pagination {
+            start: params
+                .get("start")
+                .unwrap()
+                .parse::<usize>()
+                .map_err(Error::ParseError)?,
+            end: params
+                .get("end")
+                .unwrap()
+                .parse::<usize>()
+                .map_err(Error::ParseError)?,
+        };
+        // println!("{:?}", pagination);
+        pagination = pagination.sanitize();
+        // println!("{:?}", pagination);
+        return Ok(pagination);
+    }
+    Err(Error::MissingParameters)
 }
 
 #[tokio::main]
@@ -106,12 +176,13 @@ async fn main() {
         );
 
     let get_questions = warp::get()    
-        .and(warp::path("questions"))
-        .and(warp::path::end())
-        .and(warp::query())
-        .and(store_filter)
-        .and_then(get_questions)
-        .recover(return_error);
+        .and(warp::path("questions"))   // http://localhost:3030/questions
+        // .and(warp::path("another"))   // http://localhost:3030/questions/another
+        .and(warp::path::end())         // marks the end of the path
+        .and(warp::query())             // this gets the url parameters. Sets first param.
+        .and(store_filter)              // Is this a call to a closure? Did it capture the `store` variable? Sets second param.
+        .and_then(get_questions)        // get_questions receives 2 params.    
+        .recover(return_error);         // if get_questions fails, I think...
 
     let routes = get_questions.with(cors);
 
