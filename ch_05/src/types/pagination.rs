f1b7e94e98b62e39b9a966296144d074bd2eeb84
Brandon Hernandez <bhb.xyz@outlook.com>
2025-05-16 12:07:19 -0500

    worked further on the Documentation part. Rust's comments are awesome! (cargo doc --open)

--- ch_05/src/types/pagination.rs
+++ ch_05/src/types/pagination.rs
@@ -1,13 +1,17 @@
 use std::collections::HashMap;
 use handle_errors::Error;
 
+/// Pagination struct that is getting extracted
+/// from query params
 #[derive(Debug)]
 pub struct Pagination {
+    /// The index of the first item that has to be returned
     pub start: usize,
+    /// The index of the last item that has to be returned
     pub end: usize,
 }
 
-// This one was my idea
+/// Pagination methods. These two were my idea!
 impl Pagination {
     pub fn sanitize(mut self) -> Self {
         if self.start > self.end {
@@ -27,24 +31,35 @@ impl Pagination {
     }
 }
 
-pub fn extract_pagination(params: HashMap<String, String>) -> Result<Pagination, Error> {
-    if params.contains_key("start") && params.contains_key("end") {
-        let mut pagination = Pagination {
-            start: params
-                .get("start")
-                .unwrap()
-                .parse::<usize>()
-                .map_err(Error::ParseError)?,
-            end: params
-                .get("end")
-                .unwrap()
-                .parse::<usize>()
-                .map_err(Error::ParseError)?,
-        };
-        // println!("{:?}", pagination);
-        pagination = pagination.sanitize();
-        // println!("{:?}", pagination);
-        return Ok(pagination);
-    }
-    Err(Error::MissingParameters)
+/// Extract query parameters from the `/questions` route
+/// # Example query
+/// GET requests to this route can have a pagination attached so we just
+/// return the questions we need
+/// `\questions?start=1&end=10`
+pub fn extract_pagination(params: HashMap<String, String>) 
+    -> Result<Pagination, Error> {
+        // Could be improved in the future
+        if params.contains_key("start") && params.contains_key("end") {
+            let mut pagination = Pagination {
+                // Takes the "start" parameter in the query
+                // and tries to convert it to a number
+                start: params
+                    .get("start")
+                    .unwrap()
+                    .parse::<usize>()
+                    .map_err(Error::ParseError)?,
+                // Takes the "end" parameter in the query
+                // and tries to convert it to a number
+                end: params
+                    .get("end")
+                    .unwrap()
+                    .parse::<usize>()
+                    .map_err(Error::ParseError)?,
+            };
+            // println!("{:?}", pagination);
+            pagination = pagination.sanitize();
+            // println!("{:?}", pagination);
+            return Ok(pagination);
+        }
+        Err(Error::MissingParameters)
 }
